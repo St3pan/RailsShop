30ab402ef587827c8f125a1f40924264bcc6e6a2
Stepan Khylko <stepankhylko@ukr.net>
2021-04-16 14:23:32 +0300

    devise added and navbar modified

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Users::RegistrationsController < Devise::RegistrationsController
+  private 
+
+    def sign_up_params
+      params.require(:user).permit(:first_name, :last_name, :role, :email, :password)
+    end
+
+    def account_update_params
+      logger.debug "#{@user.inspect}"
+      params.require(:user).permit(:first_name, :last_name, :role, :email, :password, :password_confirmation, :current_password)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  #devise_for :users
   resources :items
 
   resources :shops, only:[:index, :show]
 
-
+  devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
+      }
   root 'shops#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
